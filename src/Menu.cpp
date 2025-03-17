1c477e2d0bb43263c3923b00ac19ae796a02ef75
anabela17 <ana.b.bilhoto@gmail.com>
2025-03-17 16:22:05 +0000

    now missing the fuctions to execute

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -14,51 +14,65 @@ void menu() {
     std::cout<<"0.Mode"<<std::endl<<"1.source"<<std::endl<<
     "2.destination"<<std::endl<<"3.Avoid Nodes"<<std::endl<<
     "4.Avoid Segments"<<std::endl<<"5.Include Node"<<std::endl<<"6.Read From File"
-    <<std::endl<<"7.execute"<<"8.Exit Menu"<<std::endl;
+    <<std::endl<<"7.execute \n"<<"8.Exit Menu"<<std::endl;
 }
 void modedisplay() {
-    std::cout<<"Enter Mode /n:"<<"1.Driving /n"<<"2.Walking /n"<<"3.Driving-walking /n";
+    std::cout<<"Enter Mode \n"<<"1.Driving \n"<<"2.Walking \n"<<"3.Driving-walking \n";
 }
 
 void read() {
-    std::cout<<"Enter path to Locations file: /n"<<"Enter path to distance file: /n";
+    std::cout<<"Enter path to Locations file: \n"<<"Enter path to distance file: \n";
 }
 
 
 int main() {
     int choice;
-    while (choice!=8) {
+
+    while (true){
         menu();
         std::cin>>choice;
+        if (choice == 8) break;
         switch (choice) {
             case 0:
                 modedisplay();
-                std::cin>>mode;
+            std::cin>>mode;
+            break;
             case 1:
                 std::cin>>source;
             case 2:
                 std::cin>>dest;
+            break;
             case 3:
                 std::cin>>avoidNodes;
+            break;
             case 4:
                 std::cin>>avoidEdges;
+            break;
             case 5:
                 std::cin>>includeNode;
+            break;
             case 6:
                 read();
-                std::cin >> test;
-                readfromfile(test);
+            std::cin >> test;
+            readfromfile(test);
+            break;
             case 7:
                 if (mode!=0 && !source.empty() && !dest.empty() && !avoidNodes.empty() && !avoidEdges.empty()&& !includeNode.empty()) {
-
+                    switch (mode) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                    }
                 }
-                std::cout<<"One of the necessary fields was not filled /n";
+            std::cout<<"One of the necessary fields was not filled \n";
             default:
-                std::cout << "Invalid choice, please try again./n";
-                break;
+                std::cout << "Invalid choice, please try again. \n";
+            break;
 
         }
     }
-    menu();
     return 0;
 }
