bc58ba29a73276102eea9447c8a575504b581b04
Francisco Leiras <up202307823@up.pt>
2025-03-28 11:33:00 +0000

    Complete comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 
 using namespace std;
 
+/*
+ * @brief Main function to run the program
+ * @return 0 on success
+ */
 int main() {
     Graph graph;
     readData(graph);
-
 
     menu(graph);
     
